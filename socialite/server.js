1751d2041f546ed3f040e132515c74ab32dc619b
Prajneya <prajneya.kumar@research.iiit.ac.in>
2020-10-10 18:03:42 +0530

    chore: Add boilerplate for React+Express+Node

---
+++ socialite/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+  return res.send('Express Connected.');
+});
+
+app.listen(process.env.PORT || 8080);
