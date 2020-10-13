897bc982909af61f356a35a279f822c3b6a3ed5d
Dayitva Goel <dayitvagoel@gmail.com>
2020-10-13 11:25:48 +0530

    feat: Add UI for SignUp

--- socialite/src/components/Home/Home.js
+++ socialite/src/components/Home/Home.js
@@ -1,17 +1,37 @@
- 
 import React, { Component } from 'react';
 
+import "./Home.css"
 
 class Home extends Component {
 
-  render() {
-    return (
-      <div>
-      FrontEnd is live.
-      </div>
+ 	render() {
+   		return (
+      		<div className="signup-container">
+			  <div className="signup">Sign Up</div>
+			  <form action="#">
+			    <div className="email">
+			      <label for="email">Email Address</label>
+			      <br/>
+			      <input type="text" placeholder="Enter your email" />
+			    </div>
+			    <div className="password">
+			      <label for="password">Password</label>
+			      <input type="password" placeholder="Enter your password" />
+			    </div>
+                <div className="password">
+			      <label for="password">Confirm Password</label>
+			      <input type="password" placeholder="Enter your password" />
+			    </div>
+			    <button className="btn">Sign Up</button>
+			  </form>
+			  <div className="already-account">
+			    <span>Already have an account yet?</span>
+			    <button className="signin">Sign In</button>
+			  </div>
+			</div>
 
-    )
-  }
+    	)
+  	}
 }
 
 export default Home;
