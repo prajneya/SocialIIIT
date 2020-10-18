93224dc4a708a595525f0ba9bf8f63a3d301762f
Debayan <sahadebayan28@gamil.com>
2020-10-18 23:29:01 +0530

    Add new file for contentbased scoring

--- socialite/server/recosys/content.js
+++ socialite/server/recosys/content.js
@@ -3,10 +3,6 @@ const util = require("../controllers/util");
 
 //const empty = require("is-empty");
 const ex = require("express");
-var count_hostel = 0;
-var count_hosname = 0;
-
-var score = [{}];
 
 function check (a,b)
 {
@@ -18,24 +14,30 @@ function check (a,b)
 
 function host_hous(id)
 {
-	const cur = util(body);
-	hosnum = cur.hosnum;
-	hosname = cur.hosname;
-	house = cur.house;
+	const cur = util.getUserById(id);
+	var hosnum = cur.hosnum;
+	var hosname = cur.hosname;
+	var house = cur.house;
+	var count_hostel = 0;
+	var count_hosname = 0;
+	var count_house = 0;
 	const total = cur.friends.length;
 	for(i = 0; i < cur.friends.length; ++i)
 	{
 		var fren = util.getUserById(cur.friends[i]);
-		count_hostel=count_hostel+check(hosnum,fren.hosnum);
-		count_hosname=count_hosname+check(hosname,fren.hosname);
-		count_house=count_house+check(house,fren.house);
+		count_hostel = count_hostel+check(hosnum,fren.hosnum);
+		count_hosname = count_hosname+check(hosname,fren.hosname);
+		count_house = count_house+check(house,fren.house);
 	}
-	return [count_hostel/total*100, count_hosname/total*100, count_house/total*100];
+	count_hostel = count_hostel/total;
+	count_hosname = count_hosname/total;
+	count_house = count_house/total;
+	return [count_hostel, count_hosname, count_house];
 }
 
 function sporty(id)
 {
-	const cur = util(body);
+	const cur = util.getUserById(id);
 	var sports = cur.sports;
 	const sporlen = cur.sports.length;
 	const total = cur.friends.length;
@@ -51,7 +53,7 @@ function sporty(id)
 			{
 				sporarr[j]=sporarr[j]+check(sports[j],spofre[k]);	
 			}
-			sporarr[j]=sporarr[j]/total*100;
+			sporarr[j]=sporarr[j]/total;
 		}
 	}
 	return sporarr;
@@ -59,7 +61,7 @@ function sporty(id)
 
 function club(id)
 {
-	const cur = util(body);
+	const cur = util.getUserById(id);
 	var clubs = cur.clubs;
 	const clulen = cur.clubs.length;
 	const total = cur.friends.length;
@@ -75,8 +77,63 @@ function club(id)
 			{
 				cluarr[j]=cluarr[j]+check(clubs[j],clufre[k]);	
 			}
-			cluarr[j]=cluarr[j]/total*100;
+			cluarr[j]=cluarr[j]/total;
 		}
 	}
 	return cluarr;
+}
+
+function percen(id)
+{
+	const cur = util.getUserById(id);
+	var perhos = host_hous(id);
+	var persport = sporty(id);
+	var perclub = club(id);
+	const sporlen = cur.sports.length;
+	var totsport;
+	var totclub;
+	for(j = 0; j < sporlen; ++j)
+	{
+		totsport = totsport + persport[j];
+	}
+	const clulen = cur.clubs.length;
+	for(j = 0; j < clulen; ++j)
+	{
+		totclub = totclub + perclub[j];
+	}
+	total = perhos[0] + perhos[1] + perhos[2] + totclub + totsport;
+	return total; 
+}
+function scoring(a,b)
+{
+	const cur = util.getUserById(a);
+	const nonfren = util.getUserById(b);
+	var total = percen(a);
+	var perhos = host_hous(a);
+	var persport = sporty(a);
+	var perclub = club(a);
+	var score = 0;
+	score = score + perhos[0]*check(cur.hosnum,nonfren.hosnum);
+	score = score + perhos[1]*check(cur.hosname,nonfren.hosname);
+	score = score + perhos[2]*check(cur.house,nonfren.house);
+
+	const sporlenuser = cur.sports.length;
+	const sporlennon = nonfren.sports.length;
+	for(j = 0; j < sporlenuser; ++j)
+	{
+		for(k = 0; k < sporlennon; ++k)
+		{
+			score = score + persport[j]*check(cur.sports[j],nonfren.sports[k]);	
+		}
+	}
+	const clulenuser = cur.clubs.length;
+	const clulennon = nonfren.clubs.length;
+	for(j = 0; j < clulenuser; ++j)
+	{
+		for(k = 0; k < clulennon; ++k)
+		{
+			score = score + perclub[j]*check(cur.clubs[j],nonfren.clubs[k]);		
+		}
+	}
+	return score;
 }
