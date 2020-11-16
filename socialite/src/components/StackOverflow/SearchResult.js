a290b1eb26df207052bd485b2cea7d8cf2a7a967
Prajneya <prajneya.kumar@research.iiit.ac.in>
2020-11-17 03:31:42 +0530

    style: complete DoubtWire

--- socialite/src/components/StackOverflow/SearchResult.js
+++ socialite/src/components/StackOverflow/SearchResult.js
@@ -3,8 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import Parser from 'html-react-parser';
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { AuthContext } from '../../context/auth'
+import { AuthContext } from '../../context/auth';
+import Sidebar from "../Sidebar";
 
 function SearchResult(props){
 
@@ -31,14 +34,17 @@ function SearchResult(props){
 
   function searchCallback(){
     var query = document.getElementById("search_query").value;
+    if(query==""){
+      return;
+    }
     props.history.push({
       pathname: "/search",
       state: { query: query },
     });
   }
 
-  const query = props.location.state.query;
-
+  const query = props.location.state ? props.location.state.query : "";
+  console.log(props.location)
   const { data: postData } = useQuery(FETCH_POSTS_QUERY, {
         variables: {
             searchString: query
@@ -46,58 +52,83 @@ function SearchResult(props){
   });
   var post_list = postData ? postData.searchByTextPost : "";
 
+  const { data: people } = useQuery(FETCH_PEOPLE_QUERY);
+
+  var people_list = people ? people.getTopAnswered : "";
+
+  const { data: trending_tags } = useQuery(FETCH_TAGS);
+
+  var trending_tags_list = trending_tags ? trending_tags.getTopTags : "";
+
 	return (
             <>
-          		<div className="container">
-          			<div className="authenticate-nav">
-                    <div className="a-nav-right"> 
-                        <button className="rounded my-1 bg-primary" onClick={createPost}>CREATE POST</button>
-                        <button className="rounded my-1 ml-2" onClick={dashboard}>DASHBOARD</button>
-                        <button className="rounded my-1 ml-2" onClick={logUserOut}>LOGOUT</button>
+              <Sidebar/>
+              <main class="s-layout__content">
+                <div className="container-fluid">
+
+                  <div className="explore-posts pr-3">
+                    <form class="searchbox">
+                      <input type="text" id="search_query" placeholder="Search Questions" autocomplete="off"/>
+                      <button type="button" className="rounded m-2 search-button float-right" onClick={searchCallback}><i><FontAwesomeIcon icon={faSearch} /></i></button>
+                    </form>
+                    <br/> <br/> <br/> 
+                    <div class="trending-tags desktop-only">
+                      <div className="explore-subheader my-4">Trending:</div>
+                      {trending_tags_list && trending_tags_list.map(trending_tag => ( 
+                      <div className="tag px-3 py-2 mr-1 my-1">#{trending_tag['name']}</div>
+                      ))}
                     </div>
-                </div>
-
-                <div className="explore-posts p-3">
-                  <div className="explore-header my-2">Explore Topics</div>
-                  <input type="text" id="search_query" placeholder={query} />
-                  <button className="rounded m-2 bg-success float-right" onClick={searchCallback}>SEARCH</button>
-                  <div className="explore-subheader my-4">Trending:</div>
-                  <div className="tag px-3 py-2 mr-1 my-1">#community</div>
-                  <div className="tag px-3 py-2 mr-1 my-1">#community</div>
-                  <div className="tag px-3 py-2 mr-1 my-1">#community</div>
-                </div>
-
-                <hr/>
-
-                <div className="explore-posts p-3">
-                  <div className="explore-header my-2">Search Results</div>
-                  <div className="posts-list">
-                    {post_list && post_list.map(post => ( 
-                      <div className="single-post p-3" onClick={() => showIssue(post['_id'])}>
-                        <hr/>
-                        <div className="post">
-                          <div className="post-body">
-                            <div className="post-header">{post['title']}</div>
-                            <div className="post-content mt-4">{post ? Parser(post['body']) : ""}</div>
-                          </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-xl-9">
+                      <div className="explore-posts">
+                        <div className="posts-list">
+                          {post_list && post_list.map(post => ( 
+                            <div className="single-post p-3 my-2" onClick={() => showIssue(post['id'])}>
+                              <div className="post">
+                                <div className="post-body">
+                                  <div className="post-header">{post['title']}</div>
+                                  <div className="post-content mt-4">{post ? Parser(post['body']) : ""}</div>
+                                </div>
+                              </div>
+                              <hr/>
+                              <div className="question-info mt-4">
+                                <div className="tags d-inline-block text-left">
+                                  {post.tags && Object.keys(post.tags).map(tag => ( 
+                                  <div className="tag px-3 py-2 mr-1 my-1">#{tag}</div>
+                                  ))}
+                                </div>
+                                <div className="info-details text-right">
+                                  <div className="small-upvote-count d-inline-block mr-2 px-2 py-1">{post['answers'].length} answers</div>
+                                </div>
+                              </div>
+                            </div>
+                          ))} 
                         </div>
-                        <div className="question-info mt-4">
-                          <div className="tags d-inline-block">
-                            {post.tags && Object.keys(post.tags).map(tag => ( 
-                            <div className="tag px-3 py-2 mr-1 my-1">#{tag}</div>
-                            ))}
-                          </div>
-                          <div className="info-details">
-                            <div className="small-upvote-count d-inline-block mr-2 px-2 py-1">{post['answers'].length} answers</div>
-                            <div className="user-poll bg-danger text-white px-2 py-1 my-2 d-inline-block">Report</div>
-                          </div>
-                        </div>
-                        <hr/>
                       </div>
-                    ))} 
+                    </div>
+                    <div className="col-xl-3 right-sidebar desktop-only">
+                      <div className="text-center">
+                        <button className="my-1 create-post w-100 py-3" onClick={createPost}>CREATE POST + </button>
+                      </div>
+                      <div className="my-3 p-2 top-users-post">
+                        <div className="card-header">
+                          TOP USERS
+                        </div>
+                        <div className="top-users m-4">
+                          <ul>
+                          {people_list && people_list.map(person => ( 
+                            <li className="mt-4">{person['username']}
+                              <div className="no-post float-right">{person['times_answered']} posts</div>
+                            </li>
+                            ))}
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
-          		</div>
+              </main>
             </>
       )
 }
@@ -107,6 +138,20 @@ const FETCH_POSTS_QUERY = gql`
         searchByTextPost(query: $searchString){
             _id title body email answers{ id } upvotes{ id } createdAt tags
         }
+    }
+`
+
+const FETCH_PEOPLE_QUERY = gql`
+    query{
+        getTopAnswered{
+            username times_answered
+        }
+    }
+`
+
+const FETCH_TAGS = gql`
+    query{
+        getTopTags
     }
 `
 
