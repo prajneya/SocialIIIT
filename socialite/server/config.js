f71afc618efdf49fb3c512d2ed7a136eccb79f66
piyush0406 <pshroy@gmail.com>
2020-12-13 01:02:06 +0530

    FIX: Profile picture in Timeline

--- socialite/server/config.js
+++ socialite/server/config.js
@@ -1,10 +1,7 @@
 module.exports = {
-	MONGODB: "mongodb+srv://SRDewan:abcd1234@database.vvxaz.mongodb.net/Data?retryWrites=true&w=majority",
+	MONGODB: "mongodb://SRDewan:abcd1234@database-shard-00-00.vvxaz.mongodb.net:27017,database-shard-00-01.vvxaz.mongodb.net:27017,database-shard-00-02.vvxaz.mongodb.net:27017/Data?ssl=true&replicaSet=atlas-pv17s2-shard-0&authSource=admin&retryWrites=true&w=majority",
 	SECRET_KEY: "snakes key",
-	VERIFY_KEY: "Jpmcbc5!",
 	CLOUDINARY_API_KEY: "129317599251351",
 	CLOUDINARY_API_SECRET: "yZ9qGRmaJlDrZHPBlph_VudOFVs",
-	CLOUDINARY_NAME: "dmhai1riu",
-	orgmail: "socialiiit@gmail.com",
-	orgpass: "TotallySecretPassword"
+	CLOUDINARY_NAME: "dmhai1riu"
 }
