fa766dd5856c23b94ea0e81cd1fd07206d987ac3
SRDewan <shaurya.dewan@students.iiit.ac.in>
2020-12-13 19:55:30 +0530

    fix: Updated config file again.

--- socialite/server/config.js
+++ socialite/server/config.js
@@ -1,7 +1,10 @@
 module.exports = {
 	MONGODB: "mongodb://SRDewan:abcd1234@database-shard-00-00.vvxaz.mongodb.net:27017,database-shard-00-01.vvxaz.mongodb.net:27017,database-shard-00-02.vvxaz.mongodb.net:27017/Data?ssl=true&replicaSet=atlas-pv17s2-shard-0&authSource=admin&retryWrites=true&w=majority",
 	SECRET_KEY: "snakes key",
+	VERIFY_KEY: "Jpmcbch5"
 	CLOUDINARY_API_KEY: "129317599251351",
 	CLOUDINARY_API_SECRET: "yZ9qGRmaJlDrZHPBlph_VudOFVs",
-	CLOUDINARY_NAME: "dmhai1riu"
+	CLOUDINARY_NAME: "dmhai1riu",
+	orgmail: "socialiiit@gmail.com",
+	orgpass: "TotallySecretPassword"
 }
