90847206d1f0bf500dabf2cddd8c48e76b9accab
SRDewan <shaurya.dewan@students.iiit.ac.in>
2020-12-17 20:35:07 +0530

    fix: Querify url fix.

--- socialite/src/components/ErrorPages/NotFound.js
+++ socialite/src/components/ErrorPages/NotFound.js
@@ -12,7 +12,7 @@ function NotFound(props){
               <div id="error_content"><p>> <span>ERROR CODE</span>: "<i>HTTP 404 Lost in Vindhya</i>"</p>
               <p>> <span>ERROR DESCRIPTION</span>: "<i>We cannot find the page you were looking for.</i>"</p>
               <p>> <span>ERROR POSSIBLY CAUSED BY</span>: [<b>Aww, come on, Gupta Ji! We've told you not to nibble on the server wires.</b>...]</p>
-              <p>> <span>SOME PAGES ON THIS SERVER THAT YOU DO HAVE PERMISSION TO ACCESS</span>: [<a href="/">Home Page</a>, <a href="/stack-overflow">Querify</a>, <a href="/recommendations">Recommendations</a>...]</p><p>> <span>HAVE A NICE DAY SIR  :-)</span></p>
+              <p>> <span>SOME PAGES ON THIS SERVER THAT YOU DO HAVE PERMISSION TO ACCESS</span>: [<a href="/">Home Page</a>, <a href="/querify">Querify</a>, <a href="/recommendations">Recommendations</a>...]</p><p>> <span>HAVE A NICE DAY SIR  :-)</span></p>
               </div>
               
             </div>
